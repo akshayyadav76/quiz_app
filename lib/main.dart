69c817487bbdb92ba2de96608592e99a8d4b2db8
Akshay <akshayyadav7613@gmail.com>
2019-07-27 23:30:58 +0530

    did

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
 main(){
-runApp(App());
+runApp(
+    MaterialApp(
+
+      home: App(),));
 }
 
 class App extends StatelessWidget{
+ // var style=
+
+ void onPass(BuildContext context,
+     String no){
+     String no1=no;
+   var alert = AlertDialog(title: Text("${no1}result"),
+   content: Text("your anwser is wrong"),
+     actions: <Widget>[
+       RaisedButton(
+           child: Text("Ok",),
+            textColor: Colors.black,
+           onPressed: (){Navigator.of(context).pop();})],
+   );
+
+   showDialog(context: context,builder: (BuildContext context){
+     return alert;
+   });
+ }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("ss"),);
+    return  Scaffold(
+      appBar: AppBar(title: Text("Quiz App"),centerTitle: true,),
+      body: Column(children: <Widget>[
+        Text("this  what's the question?"),
+        RaisedButton(child: Text("answer 1"),onPressed: ()=> onPass(context,"answer 1")),
+        RaisedButton(child: Text("answer 2"),onPressed: null),
+        RaisedButton(child: Text("answer 3"),onPressed: null),
+        RaisedButton(child: Text("answer 4"),onPressed: null),
+      ],),
+    );
   }
 
 }
